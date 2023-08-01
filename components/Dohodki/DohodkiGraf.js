769fa1759a712a37a53d45404e281f7dd4b40103
Timotej Vošinek <timotej.vosinek@student.um.si>
2023-08-01 16:50:42 +0200

    spreminjanje barv za meje

--- components/Dohodki/DohodkiGraf.js
+++ components/Dohodki/DohodkiGraf.js
@@ -4,7 +4,8 @@ import ThemeContext from '../../ThemeContext';
 import DarkTheme from '../../DarkTheme';
 import LightTheme from '../../LightTheme';
 import { Chart, Line, Area, HorizontalAxis, VerticalAxis } from 'react-native-responsive-linechart'
-
+import { TRANSAKCIJE } from '../../data/Transakcije'
+import { date } from 'yup';
 
 
 
@@ -13,36 +14,11 @@ import { Chart, Line, Area, HorizontalAxis, VerticalAxis } from 'react-native-re
 const DohodkiGraf = ({navigation}) => {
     const { isDarkTheme } = useContext(ThemeContext);
     const theme = isDarkTheme ? DarkTheme : LightTheme;
-    const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}];
+    
     return (
         <View style={[styles.container, { backgroundColor: theme.backgroundColor}]}>
             <Text style={[styles.text, { color: theme.textColor }]} >Graf Dohodkov</Text>
-            <Chart
-                style={{ height: 200, width: 400 }}
-                data={[
-                    { x: -2, y: 15 },
-                    { x: -1, y: 10 },
-                    { x: 0, y: 12 },
-                    { x: 1, y: 7 },
-                    { x: 2, y: 6 },
-                    { x: 3, y: 8 },
-                    { x: 4, y: 10 },
-                    { x: 5, y: 8 },
-                    { x: 6, y: 12 },
-                    { x: 7, y: 14 },
-                    { x: 8, y: 12 },
-                    { x: 9, y: 13.5 },
-                    { x: 10, y: 18 },
-                ]}
-                padding={{ left: 40, bottom: 20, right: 20, top: 20 }}
-                xDomain={{ min: -2, max: 10 }}
-                yDomain={{ min: 0, max: 20 }}
-                >
-                <VerticalAxis tickCount={11} theme={{ labels: { formatter: (v) => v.toFixed(2) } }} />
-                <HorizontalAxis tickCount={5} />
-                <Area theme={{ gradient: { from: { color: '#ffa502' }, to: { color: '#ffa502', opacity: 0.4 } }}} />
-                <Line theme={{ stroke: { color: '#ffa502', width: 5 }, scatter: { default: { width: 4, height: 4, rx: 2 }} }} />
-            </Chart>
+            
         </View>
     )
 }
--- components/Meje/Meja.js
+++ components/Meje/Meja.js
@@ -7,8 +7,9 @@ import DarkTheme from '../../DarkTheme';
 const Meja = ({meja}) => {
     const { isDarkTheme } = useContext(ThemeContext);
     const theme = isDarkTheme ? DarkTheme : LightTheme;
+    const barva = (meja.max - meja.curr)< 10 ? "red": ((meja.max - meja.curr)< 100 ? "orange": "green");
     return (
-        <View style={[styles.container, { backgroundColor: theme.backgroundColor, borderBottomColor: theme.borderBottomColor }]}>
+        <View style={[styles.container, { backgroundColor: theme.backgroundColor, borderBottomColor: theme.borderBottomColor, borderRightColor:  barva, borderRightWidth: 10 }]}>
             <Text style={{color: theme.textColor}}>ID: {meja.id}</Text>
             <Text style={[styles.ime,{color: theme.textColor}]}>Name: {meja.name}</Text>
             <Text style={{color: theme.textColor}}>Max: {meja.max}</Text>
