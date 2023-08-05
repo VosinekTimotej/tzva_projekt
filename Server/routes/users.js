aa844a7e6a3c3ae297d56abd163a40672ab4a3d8
Rok Rozman <rok.rozman2@student.um.si>
2023-08-05 14:58:47 +0200

    Dodan login user

--- Server/routes/users.js
+++ Server/routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { User } = require('../models/Models');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
 
 // get all users for testing
 router.get('/', async (req, res) => {
@@ -32,5 +34,35 @@ router.post('/', async (req, res) => {
         res.status(500).json({ error: 'Internal server error' });
     }
 });
+
+router.route('/login').post(async (req,res)=>{
+    try{
+        const username = req.body.username;
+        const password = req.body.password;
+
+        // ali smo dobili username in geslo
+        if (!username || !password) {
+            return res.status(400).json({ message: 'Username and password are required' });
+        }
+
+        // poiscemo user
+        const user = await User.findOne({ username });
+
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+
+        //! SAMO TEMP DOKLER NE NAREDIMO HASHING
+        if (password !== user.password){
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+        
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+        res.status(201).json({ msg: "Logged in", token});
+    } catch (error) {
+        console.error('Error creating user:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+})
 
 module.exports = router;
