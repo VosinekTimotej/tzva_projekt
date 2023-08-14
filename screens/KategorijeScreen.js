0743ddf42c9720d92fa92e47de9e945009756e1d
Rok Rozman <rok.rozman2@student.um.si>
2023-08-14 11:40:31 +0200

    get categories

--- screens/KategorijeScreen.js
+++ screens/KategorijeScreen.js
@@ -1,5 +1,5 @@
 import { ScrollView, StyleSheet, Text, View } from 'react-native'
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import ThemeContext from '../ThemeContext';
 import DarkTheme from '../DarkTheme';
 import LightTheme from '../LightTheme';
@@ -10,14 +10,50 @@ import Kategorija from '../components/Kategorije/Kategorija';
 import AddKategorija from '../components/Kategorije/AddKategorija';
 import Footer from '../components/Footer/Footer';
 
-
-//TODO Footer 
+//TODO kategorija podrobno
+// button delete category
+// Ce vredu vrne kategorije ko jih get
+// edit category
 
 const KategorijeScreen = ({navigation}) => {
     const { isDarkTheme } = useContext(ThemeContext);
     const theme = isDarkTheme ? DarkTheme : LightTheme;
 
     const [isVisible, setIsVisible] = useState(false);
+
+    const [kategorije, setKategorije] = useState([]);
+
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                getCategories();
+            } catch (error) {
+                console.error('Error fetching categories:', error);
+            }
+        };
+
+        fetchCategories();
+    }, []);
+
+    // get categories
+    const getCategories = async () =>{
+        try {
+            const token = await AsyncStorage.getItem('userToken');
+            const headers = {
+                Authorization: `${token}` 
+            };
+            const response = await axios.get(`${apiURL}/users/category`, { headers });
+            
+            //! Stestirat 
+            const cats = response.data.categories
+            setAcc(cats)
+            // console.log("acc 0 ", accs[0])
+        } catch (error) {
+            console.log('Error: ', error)
+            Alert.alert('Error', 'Something went wrong with getting accounts!');
+        }
+        
+    }
 
     const handleAddKategorija = (data) => {
         console.log('New data:', data);
